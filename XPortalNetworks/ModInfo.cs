9b326a4917cf646049662343c32b1acf3578b834
Vapok <vapokrocks@gmail.com>
2026-09-13 16:59:35 -0400

    Updating mod version

--- XPortalNetworks/ModInfo.cs
+++ XPortalNetworks/ModInfo.cs
@@ -8,11 +8,11 @@
         public const string Author = "Vapok";
         public const string Name = "XPortalNetworks";
         public const string GitHubRepo = "Vapok/XPortalNetworks";
-        public const string Version = "1.2.25";
+        public const string Version = "1.0.0";
         public const string Description = "Select portal destination from a list of existing portals with custom networks and private portals support. No more tag pairing, and no more portal hubs!";
         public const string WebsiteUrl = "https://github.com/" + GitHubRepo;
-        public const int NexusId = 2239;
-        public const string BepInExPackVersion = "5.4.2333";
+        public const int NexusId = 3719;
+        public const string BepInExPackVersion = "5.4.2350";
         public const string JotunnVersion = Jotunn.Main.Version;
     }
 }
